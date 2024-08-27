1ecdfd862224cf006c08313537802b1cf0c57f2c
KieranMueller <kieran98mueller@gmail.com>
2024-08-27 13:23:41 -0500

    init

---
+++ src/main/java/com/kieran/azureDemoMaven/AzureDemoMavenApplication.java
@@ -0,0 +1,13 @@
+package com.kieran.azureDemoMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDemoMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDemoMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kieran/azureDemoMaven/AzureDemoMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kieran.azureDemoMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDemoMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
